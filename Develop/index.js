e7ec84e306bee75117c73fa8104393d92427689b
Francis Bond <francisbond1996@gmail.com>
2022-01-24 21:00:27 -0600

    Added packages, added code to ask for question

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "Enter your projects title",
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: "Enter your projects description",
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: "Enter installation instructions",
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: "Enter usage information",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "Enter contribution guidelines",
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: "Enter test information",
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: "Choose a license",
+        name: 'license',
+        choices: ["MIT", "GNU GPLv3", "Apache", "Mozilla Public"]
+    },
+    {
+        type: 'input',
+        message: "Enter your Github username",
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: "Enter email address",
+        name: 'email'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeToFile("README.md", generateMarkdown(response), (err) =>
+    err ? console.log(err) : console.log('Readme created') );
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            console.log(response)
+            writeToFile(response);
+        }
+)};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## How to Contribute
+
+${data.contribution}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+[Github](https://github.com/${username})
+
+Reach me with more question at ${email}
 
 `;
 }
